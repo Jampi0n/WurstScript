6768d6de9de6e0f4d8c406ca8be80c11aaa1e172
Jampi0n <27952849+Jampi0n@users.noreply.github.com>
2021-05-12 21:26:30 +0200

    detect cyclic dependencies caused by config packages

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/attributes/InitOrder.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/attributes/InitOrder.java
@@ -1,15 +1,19 @@
 package de.peeeq.wurstscript.attributes;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import de.peeeq.wurstscript.ast.WImport;
+import de.peeeq.wurstscript.ast.WImports;
 import de.peeeq.wurstscript.ast.WPackage;
 import de.peeeq.wurstscript.utils.Utils;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -61,10 +65,9 @@ public class InitOrder {
         return ImmutableList.copyOf(result);
     }
 
-    private static void collectImportedPackages(List<WPackage> callStack, WPackage p, Collection<WPackage> result) {
-        callStack.add(p);
-        for (WImport i : p.getImports()) {
-            WPackage imported = i.attrImportedPackage();
+    private static void addCollectImportedPackage(List<WPackage> callStack, WPackage p, Collection<WPackage> result, WImports imports) {
+        for (WImport i : imports) {
+            @Nullable WPackage imported = i.attrImportedPackage();
 
             if (imported == null || i.getIsInitLater()) {
                 continue;
@@ -89,12 +92,54 @@ public class InitOrder {
                 return;
             }
 
-            if (result.contains(imported)) {
-                continue;
+            if (!result.contains(imported)) {
+                result.add(imported);
+                collectImportedPackages(callStack, imported, result);
             }
-            result.add(imported);
-            collectImportedPackages(callStack, imported, result);
+            // add imports of configured package to config package
+            @Nullable WPackage configPackage = p.getModel().attrConfigOverridePackages().get(imported);
+            if (configPackage != null && configPackage != p) {
+                if (configPackage == callStack.get(0)) {
+                    String packagesMsg = Utils.join(toStringArray(callStack), " -> ");
+
+                    String msg = "Cyclic init dependency between packages: " + packagesMsg + " -> " + configPackage.getName() +
+                        "\nChange some imports to 'initlater' imports to avoid this problem.";
+                    for (WImport imp : configPackage.getImports()) {
+                        if (callStack.size() > 1 && imp.attrImportedPackage() == callStack.get(1)) {
+                            imp.addError(msg);
+                            return;
+                        }
+                    }
+                    configPackage.addError(msg);
+                    return;
+                }
+                if(!result.contains(configPackage)) {
+                    result.add(configPackage);
+                    collectImportedPackages(callStack, configPackage, result);
+                }
+            }
         }
+    }
+
+    private static void collectImportedPackages(List<WPackage> callStack, WPackage p, Collection<WPackage> result) {
+        callStack.add(p);
+        addCollectImportedPackage(callStack, p, result, p.getImports());
+        /*
+        Imports of config packages are added to the imports of the configured package.
+        Since config packages are initialized directly before the configured package,
+        all imports are merged on the configured package to ensure it is initialized at the correct time.
+        This enables importing the configured package in the config package,
+        even though the configured package will be initialized after the config package.
+         */
+        for(Map.Entry<WPackage, WPackage> e: p.getModel().attrConfigOverridePackages().entrySet()) {
+            if(e.getValue().equals(p)) {
+                addCollectImportedPackage(callStack, e.getKey(), result, e.getKey().getImports());
+            }
+        }
+        /*WPackage configPackage = p.getModel().attrConfigOverridePackages().get(p);
+        if (configPackage != null) {
+            addCollectImportedPackage(callStack, p, result, configPackage.getImports());
+        }*/
         callStack.remove(callStack.size() - 1);
     }
 
